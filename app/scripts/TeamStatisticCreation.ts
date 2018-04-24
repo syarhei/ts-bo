a926e8b55bfc189b5358baa56d17f9fbd93a4558
Sergei Murkou <murkous@mail.ru>
2018-04-24 07:53:35 +0300

    refactor script for create teams' statistics

--- app/scripts/TeamStatisticCreation.ts
+++ app/scripts/TeamStatisticCreation.ts
@@ -3,8 +3,8 @@ import uuid = require("uuid");
 import {MatchCategory} from "../src/contracts/MatchCategory";
 import container from "../inversify/config";
 import {DATABASE_CONTEXT} from "../inversify/identifiers/common";
-import {DBContext} from "../src/DB/DBContext";
 import {Match} from "../src/contracts/Match";
+import {Op} from "sequelize";
 
 interface TeamValue {
     key: string;
@@ -28,27 +28,57 @@ interface MatchValue {
 const { clubs }: { name: string, clubs: TeamValue[] } = require("../../statistics/2012-13/en.1.clubs.json");
 const BPL: { name: string, rounds: Round[] } = require("../../statistics/2012-13/en.1.json");
 
-let categoryId: string = uuid();
-let teams: Team[] = null;
 const COUNTRY: string = "England";
-const matchValues: MatchValue[] = [];
-BPL.rounds.forEach(round => {
-    matchValues.push(...round.matches);
+let categoryId: string = uuid();
+
+const teams: Team[] = clubs.map(club => {
+    return {
+        id: uuid(),
+        name: club.name,
+        year: 2018,
+        country: COUNTRY,
+        owner: "Siarhei Murkou"
+    } as Team;
 });
 
-const context = container.get<DBContext>(DATABASE_CONTEXT);
+const matchValues: MatchValue[] = [];
+BPL.rounds.forEach((round, index, rounds) => {
+    if (index !== rounds.length - 1) {
+        matchValues.push(...round.matches);
+    }
+});
+const matches: Match[] = matchValues.map(match => {
+    return {
+        id: uuid(),
+        matchCategoryId: categoryId,
+        teamHomeId: teams.find(team => team.name === match.team1.name).id,
+        teamGuestId: teams.find(team => team.name === match.team2.name).id,
+        date: Date.parse(match.date),
+        place: COUNTRY,
+        homeGoals: match.score1,
+        guestGoals: match.score2
+    } as Match;
+});
+
+export function getMatchesOfLastRound(): Match[] {
+    return BPL.rounds[BPL.rounds.length - 1].matches.map(match => {
+        return {
+            id: uuid(),
+            matchCategoryId: categoryId,
+            teamHomeId: teams.find(team => team.name === match.team1.name).id,
+            teamGuestId: teams.find(team => team.name === match.team2.name).id,
+            date: Date.parse(match.date),
+            place: COUNTRY,
+            homeGoals: match.score1,
+            guestGoals: match.score2
+        } as Match;
+    })
+}
+
+const context: any = container.get(DATABASE_CONTEXT);
 
 async function createTeams() {
     try {
-        teams = clubs.map(club => {
-            return {
-                id: uuid(),
-                name: club.name,
-                year: 2018,
-                country: COUNTRY,
-                owner: "Siarhei Murkou"
-            } as Team;
-        });
         await context.TEAM.bulkCreate(teams);
     } catch (err) {
         console.log(`Error during Team creation`);
@@ -74,18 +104,6 @@ async function createMatchCategory() {
 
 async function createMatches() {
     try {
-        const matches: Match[] = matchValues.map(match => {
-            return {
-                id: uuid(),
-                matchCategoryId: categoryId,
-                teamHomeId: teams.find(team => team.name === match.team1.name).id,
-                teamGuestId: teams.find(team => team.name === match.team2.name).id,
-                date: Date.parse(match.date),
-                place: COUNTRY,
-                homeGoals: match.score1,
-                guestGoals: match.score2
-            } as Match;
-        });
         await context.MATCH.bulkCreate(matches);
     } catch (err) {
         console.log(`Error during Match creation`);
@@ -93,25 +111,29 @@ async function createMatches() {
     }
 }
 
-export async function createFootballSet(): Promise<{ teams: Team[], categoryId: string }> {
-    try {
-        await createTeams();
-        await createMatchCategory();
-        await createMatches();
-        console.log("Football Statistic is uploaded to database");
-        return { teams, categoryId };
-    } catch (err) {
-        console.log(err);
-    }
+export async function deleteFootballSet(matchIds: string[], categoryId: string, teamIds: string[]) {
+    await context.MATCH.destroy({ where: { id: { [Op.or]: matchIds } } });
+    await context.MATCH_CATEGORY.destroy({ where: { id: categoryId } });
+    await context.TEAM.destroy({ where: { id: { [Op.or]: teamIds } } });
+    console.log("Football Statistic is deleted from database");
 }
 
-(async () => {
-    try {
-        if (require.main === module) {
-            await createFootballSet();
-            process.exit(0);
-        }
-    } catch (err) {
-        process.exit(-1);
-    }
-})();
+export async function createFootballSet(): Promise<{ teams: Team[], categoryId: string, matches: Match[] }> {
+    await createTeams();
+    await createMatchCategory();
+    await createMatches();
+    console.log("Football Statistic is uploaded to database");
+    return { teams, categoryId, matches };
+}
+
+// (async () => {
+//     try {
+//         if (require.main === module) {
+//             await createFootballSet();
+//             process.exit(0);
+//         }
+//     } catch (err) {
+//         console.log(err);
+//         process.exit(-1);
+//     }
+// })();
