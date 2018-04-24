7921c9d7ad795b6b7baab50ec335853d172acb10
Sergei Murkou <murkous@mail.ru>
2018-04-24 20:23:13 +0300

    adjust logger writings usage colors

--- app/utils/logger.ts
+++ app/utils/logger.ts
@@ -5,21 +5,30 @@ import {RequestHandler} from "express-serve-static-core";
 export function logRequestResponse(req: Request, res: Response, next: NextFunction): RequestHandler {
     console.log(`[${req.method.blue.bold}]: ${req.path}`);
     const str: string = "";
+    res.json = new Proxy(res.json, {
+        apply: (target, thisArg, argArray) => {
+            const status: string = res.statusCode.toString();
+            if (status.startsWith("4")) {
+                console.log(`${"    ERROR:".magenta.bold} "${(argArray[0] as Error).message.magenta.bold}"`);
+            }
+            if (status.startsWith("5")) {
+                console.log(`${thisArg[0]}`.cyan.bold);
+            }
+            target.apply(thisArg, argArray);
+        }
+    });
     res.end = new Proxy(res.end, {
         apply: ((target, thisArg, argArray) => {
             const status: string = res.statusCode.toString();
             if (status.startsWith("2") || status.startsWith("3")) {
                 console.log(`    ${"STATUS:".bold} ${res.statusCode.toString().green.bold}`);
             } else if (status.startsWith("4") || status.startsWith("5")) {
-                if (status.startsWith("4")) {
-                    const errorMessage: string = `${"    ERROR:".red.bold} ${(thisArg as Error).message.bold}`;
-                    console.log(errorMessage);
-                }
-                console.log(`    ${"STATUS:".bold} ${res.statusCode.toString().green.bold}`);
+                console.log(`    ${"STATUS:".bold} ${res.statusCode.toString().red.bold}`);
             }
             target.apply(thisArg, argArray);
         })
     });
+
     next();
     return;
 }
