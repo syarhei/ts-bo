403c82f000ca902abd0bfe1fd180bb4dde082779
Sergei Murkou <murkous@mail.ru>
2018-04-24 09:46:25 +0300

    add colors library for logger

--- app/utils/logger.ts
+++ app/utils/logger.ts
@@ -1,11 +1,17 @@
 import {NextFunction, Request, Response} from "express";
 import {RequestHandler} from "express-serve-static-core";
+import * as colors from "colors/safe";
 
 export function logRequestResponse(req: Request, res: Response, next: NextFunction): RequestHandler {
-    console.log(`[${req.method}]: ${req.path}`);
+    console.log(`[${(colors.blue as any).bold(req.method)}]: ${req.path}`);
     res.end = new Proxy(res.end, {
         apply: ((target, thisArg, argArray) => {
-            console.log(`    STATUS: ${res.statusCode}`);
+            const status: string = res.statusCode.toString();
+            if (status.startsWith("2") || status.startsWith("3")) {
+                console.log(colors.bold(`    STATUS: ${(colors.green as any).bold(res.statusCode.toString())}`));
+            } else if (status.startsWith("4") || status.startsWith("5")) {
+                console.log(colors.bold(`    STATUS: ${(colors.red as any).bold(res.statusCode.toString())}`));
+            }
             target.apply(thisArg, argArray);
         })
     });
