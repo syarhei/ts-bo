0adf19ba9a2c3b072389591e9f034e7915fb0514
Sergei Murkou <murkous@mail.ru>
2018-04-24 07:50:25 +0300

    adjust conversation method from math utils

--- app/utils/math.ts
+++ app/utils/math.ts
@@ -1,9 +1,20 @@
+import * as _ from "underscore";
 
 export function convertSumToInteger (target: number, ...args: number[]): number[] {
+    let counter: number = 0;
     let sum: number = args.reduce((memory: number, value: number) => memory + value, 0);
-    while (sum !== target) {
+    while (sum !== target && counter < 100) {
         args = args.map(value => value / sum);
         sum = args.reduce((memory: number, value: number) => memory + value, 0);
+        counter++;
+    }
+    if (sum !== target) {
+        const difference: number = target - sum;
+        const maxValue: number = _.max(args);
+        const indexOfMaxValue: number = _.indexOf(args, maxValue);
+        if (indexOfMaxValue !== -1) {
+            args[indexOfMaxValue] += difference;
+        }
     }
     return args;
 }
